c23dabd4042e6881cce937fa28421922501acfe9
Max Wang <mwang@1080agile.com>
2018-08-15 15:59:30 +1000

    init

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Nice'},
+    { id: 2, name: 'Narco'},
+    { id: 3, name: 'Celeritas'},
+    { id: 4, name: 'Magneta'},
+    { id: 5, name: 'Dynama'},
+    { id: 6, name: 'Magma'}
+];
